70f9a7fea2764965c6aa53e0e7729e8468bdac0e
JonnyNice <116230053+JonnyNice@users.noreply.github.com>
2022-10-31 15:42:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+var multiply = (num1 * num2);
 
-// Write your code here
+var random = Math.floor(Math.random() + 1 );
+
+let num3 = 4;
+let num4 = 8;
+var mod = (num3 % num4);
+
+var max = Math.max(1,20);
